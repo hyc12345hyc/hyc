554f425fecc3405eff7df9e854ea04a20e16aa0e
hyc12345hyc <31230222+hyc12345hyc@users.noreply.github.com>
2017-12-18 21:19:36 +0800

    1

--- py_basic_exercises/chapter2/2-3xuliechengfa.py
+++ py_basic_exercises/chapter2/2-3xuliechengfa.py
@@ -7,3 +7,11 @@ screen_width = 80
 text_width = len(sentense)
 box_width = text_width + 6
 left_margin = (screen_width - box_width) // 2
+
+print
+print ' ' * left_margin + '+' + '-' * (box_width - 2) + '+'
+print ' ' * (left_margin + 4) + '|' + ' ' * text_width + '|'
+print ' ' * (left_margin + 4) + '|' + sentense + '|'
+print ' ' * (left_margin + 4) + '|' + ' ' * text_width + '|'
+print ' ' * left_margin + '+' + '-' * (box_width - 2) + '+'
+print
