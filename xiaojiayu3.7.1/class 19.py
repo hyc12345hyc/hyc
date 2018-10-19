1e7354fcc7abb22039162a48a05500cecdcc7bd0
hyc12345hyc <1902505610@qq.com>
2018-10-19 17:04:30 +0800

    1

--- xiaojiayu3.7.1/class 19.py
+++ xiaojiayu3.7.1/class 19.py
@@ -8,3 +8,42 @@ def pre():
 next()
 print('-----')
 
+#1判断传入的字符串参数是否是回文联
+
+def huiwen(sen):
+    for i in range(len(sen) // 2):
+        if sen[i] == sen[-i - 1]:
+            x = 0
+        else:
+            x = 1
+    if x == 0:
+        return('yes')
+    else:
+        return('no')
+
+print(huiwen('上海自来水来自海上'))
+print('-----')
+
+#2编写一个函数，分别统计出传入字符串参数(可能不止一个参数)的英文字母、空格、数字和其他字符的个数
+def count(*para):
+    for i in range(len(para)):
+        letters = 0
+        spaces = 0
+        digits = 0
+        others = 0
+        for j in para[i]:
+            if j.isalpha():
+                letters += 1
+            elif j.isdigit():
+                digits += 1
+            elif j == ' ':
+                spaces += 1
+            else:
+                others += 1
+        print('第%d个字符串有英文字母%d、空格%d、数字%d和其他字符%d'%(i+1,letters,spaces,digits,others))
+
+count('csc231  ac','cas  ccccc233 qccef34 5ccc')
+
+
+
+
---
+++ xiaojiayu3.7.1/class 20.py
@@ -0,0 +1,37 @@
+#函数嵌套示例
+def f1():
+    def f2():
+        def f3():
+            print('from f3')
+        f3()
+        print('from f2')
+    f2()
+    print('from f1')
+f1()
+print('-----')
+
+#global关键字举例
+#在函数中修改全局变量的值
+count = 10
+def f():
+    global count
+    count = 5
+    print(count)
+f()
+print(count)
+print('-----')
+
+#nonlocal关键字举例
+#在嵌套的函数中，在内部函数修改外部函数的局部变量
+def f():
+    x = 5
+    def g():
+        nonlocal x
+        x *= x
+        return x
+    return g()
+
+print(f())
+print('-----')
+
+
