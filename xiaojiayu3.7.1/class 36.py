cbbcff873ea8a3c6aa4a58a5035773b87276cf21
hyc12345hyc <1902505610@qq.com>
2019-03-08 17:07:12 +0800

    1

--- xiaojiayu3.7.1/class 36.py
+++ xiaojiayu3.7.1/class 36.py
@@ -3,8 +3,8 @@ class Person:
     name = "turtle"
     def printname(self):
         print(self.name)
-
-Person().printname()
+a = Person()
+a.printname()
 
 #定义一个矩形类
 class Rect:
@@ -12,10 +12,18 @@ class Rect:
     width = 4
     def setRect(self):
         self.length = int(input('请输入矩形长度：'))
-        self.width =  int(input('请输入矩形宽度：'))
+        self.width = int(input('请输入矩形宽度：'))
     def getRect(self):
         print('矩形长度是%d米，宽度是%d米'%(self.length,self.width))
     def getArea(self):
         area = self.length * self.width
         print('矩形的面积是%d'%(area))
+old = Rect()
+old.getRect()
+old.getArea()
+
+new = Rect()
+new.setRect()
+new.getRect()
+new.getArea()
 
