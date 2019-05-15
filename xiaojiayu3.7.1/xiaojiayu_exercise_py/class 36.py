7e1ffb8098b51638b2d84a2d648c333b0c51715f
hyc12345hyc <1902505610@qq.com>
2019-05-15 17:35:05 +0800

    1

--- xiaojiayu3.7.1/xiaojiayu_exercise_py/class 36.py
+++ xiaojiayu3.7.1/xiaojiayu_exercise_py/class 36.py
@@ -2,29 +2,29 @@
 #0 定义一个Person类
 class Person:
     name = "turtle"
-    def printname(self):
+    def print_name(self):
         print(self.name)
 a = Person()
-a.printname()
+a.print_name()
 
 #定义一个矩形类
 class Rect:
     length = 5
     width = 4
-    def setRect(self):
+    def set_rect(self):
         self.length = int(input('请输入矩形长度：'))
         self.width = int(input('请输入矩形宽度：'))
-    def getRect(self):
+    def get_rect(self):
         print('矩形长度是%d米，宽度是%d米'%(self.length,self.width))
-    def getArea(self):
+    def get_area(self):
         area = self.length * self.width
-        print('矩形的面积是%d'%(area))
+        print('矩形的面积是%d'% area )
 old = Rect()
-old.getRect()
-old.getArea()
+old.get_rect()
+old.get_area()
 
 new = Rect()
-new.setRect()
-new.getRect()
-new.getArea()
+new.set_rect()
+new.get_rect()
+new.get_area()
 
--- xiaojiayu3.7.1/xiaojiayu_exercise_py/class 37_1.py
+++ xiaojiayu3.7.1/xiaojiayu_exercise_py/class 37_1.py
@@ -9,3 +9,98 @@
 # 当乌龟和鱼坐标重叠，乌龟吃掉鱼，乌龟体力增加20
 # 鱼暂不计算体力
 # 当乌龟体力值为0（挂掉）或者鱼儿的数量为0游戏结束
+import random as r
+#限制游戏场景范围
+area_x = [0,10]
+area_y = [0,10]
+
+class Turtle:
+    def __init__(self):
+        #初始体力
+        self.health = 100
+        #初始位置随机
+        self.x = r.randint(area_x[0], area_x[1])
+        self.y = r.randint(area_y[0], area_y[1])
+    def move(self):
+        # 随机计算方向并移动到新的位置（x, y）
+        new_x = self.x + r.choice([-1, -2, 1, 2])
+        new_y = self.y + r.choice([-1, -2, 1, 2])
+        # 检查移动后是否超出场景x轴边界
+        if new_x < area_x[0]:
+            self.x = -new_x
+        elif new_x > area_x[1]:
+            self.x = area_x[1] - (new_x - area_x[1])
+        else:
+            self.x = new_x
+        # 检查移动后是否超出场景y轴边界
+        if new_y < area_y[0]:
+            self.y = -new_y
+        elif new_y > area_y[1]:
+            self.y = area_y[1] - (new_y - area_y[1])
+        else:
+            self.y = new_y
+        # 体力消耗
+        self.health -= 1
+        # 返回移动后的新位置
+        return self.x, self.y
+    def eat(self):
+        self.health += 20
+        if self.health > 100:
+            self.health = 100
+
+class Fish:
+    def __init__(self):
+        self.x = r.randint(area_x[0], area_x[1])
+        self.y = r.randint(area_y[0], area_y[1])
+    def move(self):
+        new_x = self.x + r.choice([-1, 1])
+        new_y = self.y + r.choice([-1, 1])
+        if new_x < area_x[0]:
+            self.x = -new_x
+        elif new_x > area_x[1]:
+            self.x = area_x[1] - (new_x - area_x[1])
+        else:
+            self.x = new_x
+        if new_y < area_y[0]:
+            self.y = -new_y
+        elif new_y > area_y[1]:
+            self.y = area_y[1] - (new_y - area_y[1])
+        else:
+            self.y = new_y
+        return self.x, self.y
+
+turtle = Turtle()
+fish = []
+for i in range (10):
+    new_fish = Fish()
+    fish.append(new_fish)
+
+while True:
+    if not len(fish):
+        print('turtle win')
+        break
+    elif not turtle.health:
+        print('fish win')
+        break
+
+    pos = turtle.move()
+    # 在迭代器中删除列表元素是非常危险的，经常会出现意想不到的问题，因为迭代器是直接引用列表的数据进行引用
+    # 这里我们把列表拷贝给迭代器，然后对原列表进行删除操作就不会有问题了^_^
+    for each_fish in fish[:]:
+        if each_fish.move() == pos:
+            # 鱼被吃了
+            turtle.eat()
+            fish.remove(each_fish)
+            print('一条鱼被吃了')
+
+
+
+
+
+
+
+
+
+
+
+
